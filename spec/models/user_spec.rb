a53043b3d18a883b9f6209fddfac5e3a7bea4349
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-03-19 15:08:46 +0900

    require factorybot した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,8 @@ require 'spec_helper'
 
 describe "User" do
   describe '#create' do
-    let(:user) { create(:user) }
     it "is invalid without a nickname" do
-      user = build(:user, nickname: nil)
-      user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      expect(FactoryBot.build(:user)).to be_invalid  
     end
   end
 end
