dc08a4718ef71226d17c4299125f222654bf9c64
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-03-19 20:50:40 +0900

    初めてテスト通ったのでcommit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 describe "User" do
   describe '#create' do
-    it "is invalid without a nickname" do
-      user = FactoryBot.build(:user, nickname: "nil")
+    it 'is valid' do
+      user = build(:user)
       user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")
+      expect(user).to be_valid
     end
+
   end
 end
