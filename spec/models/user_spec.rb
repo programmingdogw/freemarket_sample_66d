6754da35777f4d242ad0297a50097bc9fb44bfcf
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-03-19 20:39:15 +0900

    テストに値入れた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ describe "User" do
   describe '#create' do
     it "is invalid without a nickname" do
       user = FactoryBot.build(:user, nickname: "nil")
-      binding.pry
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
