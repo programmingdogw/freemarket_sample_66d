ab09d1529b45f318defad3d9efeb179dcc77722a
irohaas <ginirohas@gmail.com>
2020-03-21 01:36:50 +0900

    自動デプロイ用ファイル編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+# config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.12.0'
 
@@ -15,7 +16,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/frimasample.pem'] 
+                  keys: ['~/.ssh/freemarketsample.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.180.106.20', user: 'ec2-user', roles: %w{app db web}
+server '18.180.118.52', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
