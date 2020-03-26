b81bfdc7818b7cd6e8c323b86f76282a2a688a4f
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-03-26 18:16:06 +0900

    ancestryの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_041045) do
+ActiveRecord::Schema.define(version: 2020_03_26_091148) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_03_13_041045) do
     t.text "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,11 @@ Brand.create(
   name: 'シャネル'
 )
 
-Category.create(
-  name: 'レディース'
-)
 
+lady = Category.create(:name=>"レディース")
 
-Category.create(
-  name: 'メンズ'
-)
+lady_tops = lady.children.create(:name=>"トップス")
+lady_jacket = lady.children.create(:name=>"ジャケット/アウター")
+
+lady_tops.children.create([{:name=>"Tシャツ/カットソー(半袖/袖なし)"}, {:name=>"Tシャツ/カットソー(七分/長袖)"},{:name=>"その他"}])
+lady_jacket.children.create([{:name=>"テーラードジャケット"}, {:name=>"ノーカラージャケット"}, {:name=>"Gジャン/デニムジャケット"},{:name=>"その他"}])
