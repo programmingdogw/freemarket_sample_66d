09defae21b09957bc56b2710eab489f087e12209
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-03-28 21:52:56 +0900

    ちょっとコメント多めだが、jsを整理

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -10,15 +10,17 @@ $(document).on('turbolinks:load', ()=> {
     return html;
   };
 
-  const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
-    return html;
-  };
+  // 一旦画像のビルドコメントアウト
+  // const buildImg = (index, url)=> {
+  //   const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+  //   return html;
+  // };
 
 
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+
   // 既に使われているindexを除外
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
@@ -26,21 +28,32 @@ $(document).on('turbolinks:load', ()=> {
   // 削除ボックス隠しとく
   $('.hidden-destroy').hide();
 
-
+  // フォームに変更があると次のフォームを作成
   $('#image-box').on('change', '.js-file', function(e) {
-    // fileIndexの先頭の数字を使ってinputを作る
+  
     $('#image-box').append(buildFileField(fileIndex[0]));
     fileIndex.shift();
-    //shiftメソッドは配列の先頭を削除する
+
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-    //pushは配列に要素を付け足す。今回は末尾に１足した数字。
+    
   });
   
-
+  // 削除を押すとjs-file_groupを削除
   $('#image-box').on('click', '.js-remove', function() {
     $(this).parent().remove();
     // 画像入力欄が0個にならないようにしておく
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+  });
+
+
+  // 削除を押すと:_destroyを送るためのチェックボックスにこっそりチェックが入る
+  $('#image-box').on('click', '.js-remove', function() {
+    const targetIndex = $(this).parent().data('index')
+    // 該当indexを振られているチェックボックスを取得する
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    // もしチェックボックスが存在すればチェックを入れる
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
+    (省略)
   });
 
   
