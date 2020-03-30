887db6e647d1ead4ebfa250c929a680aca8d6f87
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-03-30 15:07:29 +0900

    js弄る前にコミット

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -68,6 +68,8 @@ $(document).on('turbolinks:load', ()=> {
   });
 
 
+  // こっから先はカテゴリーのフォームに関する記述
+
   // カテゴリーセレクトボックスのオプションを作成
   function appendOption(category){
     var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
