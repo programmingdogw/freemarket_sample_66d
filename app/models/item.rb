fc18de0b8d55c144ca972047bd3aac467876cef1
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-01 10:42:10 +0900

    価格のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
 
 
   validates :name, presence: true, length: { maximum: 40 }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :condition_id, presence: true
   validates :description, presence: true, length: { maximum: 1000 }
   validates :size_id, presence: true
