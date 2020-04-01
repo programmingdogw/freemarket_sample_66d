3f4e82b38b3b24f5055bf7dd724e9246cb005636
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-01 14:02:15 +0900

    マイグレーションファイルに追記

--- db/schema.rb
+++ db/schema.rb
@@ -91,8 +91,8 @@ ActiveRecord::Schema.define(version: 2020_03_26_091148) do
     t.boolean "auction"
     t.boolean "dealing"
     t.boolean "sold"
-    t.text "parentcategory"
-    t.integer "childcategory"
+    t.text "parentcategory", null: false
+    t.integer "childcategory", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
