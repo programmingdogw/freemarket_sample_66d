b480a576648acb9c64ba1c74ab646558c9aea593
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-08 11:57:12 +0900

    途中経過

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,6 +54,26 @@ before_action :set_item, except: [:index, :new, :create, :get_category_children,
 
   def edit
     @item = Item.find(params[:id])
+     #セレクトボックスの初期値設定
+     @category_parent_array = ["---"]
+     #データベースから、親カテゴリーのみ抽出し、配列化
+     Category.where(ancestry: nil).each do |parent|
+       @category_parent_array << parent.name
+     end
+ 
+     # 親カテゴリーが選択された後に動くアクション
+     def get_category_children
+       #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+       @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+     end
+ 
+     # 子カテゴリーが選択された後に動くアクション
+     def get_category_grandchildren
+       #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+       @category_grandchildren = Category.find("#{params[:child_id]}").children
+     end
+
+
   end
 
   def update
