c6bbfd790b94a3471e9205082c6166c5bade69b1
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-04-08 16:30:36 +0900

    revert

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -72,19 +72,8 @@ before_action :set_item, except: [:index, :new, :create, :get_category_children,
      end
 
     #  実験中の記述
-    @edit_category_children = Category.find_by(name:@item.parentcategory, ancestry:nil).children
-    @edit_category_grandchildren = Category.find_by(id:@item.childcategory).children
-    # 親カテゴリーが選択された後に動くアクション
-    def edit_category_children
-      #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-      @edit_ategory_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-    end
-
-    # 子カテゴリーが選択された後に動くアクション
-    def edit_category_grandchildren
-      #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-      @edit_category_grandchildren = Category.find("#{params[:child_id]}").children
-    end
+    @editpage_category_children = Category.find_by(name:@item.parentcategory, ancestry:nil).children
+    @editpage_category_grandchildren = Category.find_by(id:@item.childcategory).children
 
   end
 
--- app/views/items/edit_category_children.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array! @edit_category_children do |child|
-  json.id child.id
-  json.name child.name
-end
