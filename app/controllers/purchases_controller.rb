d5694f429cdd80307afe6cc3b543e988818e9e3d
irohaas <ginirohas@gmail.com>
2020-05-11 11:32:39 +0900

    formを使ったsoldout機能実装途中

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,7 +8,7 @@ class PurchasesController < ApplicationController
   def index
     if @card.blank?
       #登録された情報がない場合にカード登録画面に移動
-      redirect_to new_CreditCard_path
+      redirect_to new_credit_card_path
     else
       Payjp.api_key = Rails.application.credentials.payjp[:payjp_private_key]
       #保管した顧客IDでpayjpから情報取得
